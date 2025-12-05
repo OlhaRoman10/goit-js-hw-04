2be688df96c3a31e9e6057c5316928773510df45
Olha_Roman <ovgrozdova79@gmail.com>
2025-12-05 14:43:36 +0100

    object2

--- js/task-4.js
+++ js/task-4.js
@@ -5,6 +5,19 @@ const user = {
   hobbies: ["swimming", "music", "sci-fi"],
 };
 
-
 const lastHobbie = user.hobbies.length - 1;
 console.log(user.hobbies[lastHobbie]); // "sci-fi"
+
+const book = {
+  author: "Bernard Cornwell",
+  genres: ["historical prose", "adventure"],
+  rating: 8.38,
+};
+const keys = Object.keys(book);
+
+for (const key of keys) {
+  console.log(key); // Ключ
+  console.log(book[key]); // Значення властивості
+}
+// "author", "genres", "rating"
+// "Bernard Cornwell", ["historical prose", "adventure"], 8.38
