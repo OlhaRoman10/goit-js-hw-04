df2ed5ad2ad55d6d802c2754cce91ffcc0ac26ae
Olha_Roman <ovgrozdova79@gmail.com>
2025-12-27 15:46:53 +0100

    this2

--- js/task-4.js
+++ js/task-4.js
@@ -22,18 +22,36 @@ for (const key of keys) {
 // "author", "genres", "rating"
 // "Bernard Cornwell", ["historical prose", "adventure"], 8.38
 
+// const bookShelf = {
+//   books: [
+// 		{ title: "The Last Kingdom", rating: 8 }, 
+// 		{ title: "The Mist", rating: 6 }
+// 	],
+//   getBooks() {
+//     return this.books;
+//   },
+//   addBook(newBook) {
+//     this.books.push(newBook);
+//   }
+// };
+
+// bookShelf.addBook({ title: "Dream Guardian", rating: 9 });
+// console.log(bookShelf.getBooks());
+
 const bookShelf = {
   books: [
-		{ title: "The Last Kingdom", rating: 8 }, 
-		{ title: "The Mist", rating: 6 }
-	],
-  getBooks() {
-    return this.books;
+    { title: "The Last Kingdom", rating: 8 },
+    { title: "The Mist", rating: 6 },
+  ],
+  getAvarageRating() {
+    let totalRating = 0;
+
+    for (const book of this.books) {
+      totalRating += book.rating;
+    }
+
+    return totalRating / this.books.length;
   },
-  addBook(newBook) {
-    this.books.push(newBook);
-  }
 };
 
-bookShelf.addBook({ title: "Dream Guardian", rating: 9 });
-console.log(bookShelf.getBooks());
+bookShelf.getAvarageRating(); // 7
